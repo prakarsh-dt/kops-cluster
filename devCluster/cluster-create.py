d0f0c2d47006fb848e681a4b1635dd35dbf48c6b
Abhinav-26 <abhinavdubey616@gmail.com>
2022-01-28 15:33:08 +0530

    automated few changes

--- devCluster/cluster-create.py
+++ devCluster/cluster-create.py
@@ -3,21 +3,37 @@
 # Generate random cluster name and attach with Domain name
 # Apply config file
 # Execute the kops commands
+from time import sleep
 import yaml
 import random
 import string
 import subprocess as sp
 import os
 
+def banner(heading):
+    print("-"*25)
+    print(heading)
+    print("-"*25)
+
+
 chars = string.ascii_lowercase
 definedText =  'test.' + ''.join(random.choice(chars) for i in range(5)) + '.'
+
+banner("Give Your Desired Values")
 
 domianName  = input("Enter Your Cluster Domain : ")
 clusterName = definedText + domianName
 
 region = input("Enter the Region for Cluster : ")
 
-# Cluster Config
+stateStore = input("Enter the bucket name to store Cluster Configs :")
+stateStore = "s3://{}".format(stateStore)
+
+print("Your Entered bucket is :", stateStore)
+os.system("sleep 1")
+os.environ['KOPS_STATE_STORE'] = stateStore
+
+# # # Cluster Config
 with open('cluster.yaml', 'r') as f:
     dataCluster = yaml.safe_load(f)
 
@@ -30,7 +46,7 @@ dataCluster['spec']['subnets'][0]['zone'] = region
 with open('cluster.yaml', 'w') as f:
     yaml.dump(dataCluster, f)
 
-# Master Node
+# # # Master Node
 with open('master.yaml', 'r') as f:
     dataMaster = yaml.safe_load(f)
 
@@ -42,7 +58,7 @@ dataMaster['spec']['subnets'][0] = region
 with open('master.yaml', 'w') as f:
     yaml.dump(dataMaster, f)
 
-# Worker Node
+# # # Worker Node
 with open('worker.yaml', 'r') as f:
     dataWorker = yaml.safe_load(f)
 
@@ -51,9 +67,18 @@ dataWorker['spec']['subnets'][0] = region
 with open('worker.yaml', 'w') as f:
     yaml.dump(dataWorker, f)
 
+banner("Generating Cluster-Config File")
+
 # Creating Cluster Config File 
-sp.getoutput('echo "\n---" >> cluster.yaml')
-sp.getoutput('echo "\n---" >> master.yaml')
+if sp.getoutput("cat cluster.yaml | tail -n 1") == "---":
+    print("cluster.yaml file configured for merging")
+else:
+    sp.getoutput('echo "\n---" >> cluster.yaml')
+
+if sp.getoutput("cat master.yaml | tail -n 1") == "---":
+    print("master.yaml file configured for merging")
+else:
+    sp.getoutput('echo "\n---" >> master.yaml')
 
 
 # files = sp.getoutput("ls -1 *.yaml | awk '{print $1}'")
@@ -69,19 +94,31 @@ for i in op:
     # i = i.decode("utf-8")
     # print(i)
     if i == "cluster-config.yaml\n".encode("utf-8"):
-        # print("found the file")
-        sp.getoutput('rm cluster-config.yaml')
-        sp.getoutput('cat cluster.yaml master.yaml worker.yaml >> cluster-config.yaml')
+        # print("Found the file")
+        os.system("rm cluster-config.yaml")
+        print("\nCreating Config File\n")
+        os.system("sleep 2")
+        os.system("cat cluster.yaml master.yaml worker.yaml >> cluster-config.yaml")
         break
-    else:
-        print("Not Found")
+    elif i != "cluster-config.yaml\n".encode("utf-8"):
+        # print("File Not Found")
+        print("\nCreating new Config File\n")
+        os.system("sleep 2")
+        os.system("cat cluster.yaml master.yaml worker.yaml >> cluster-config.yaml")
+        break
 
 # print("cluster-config.yaml\n".encode("utf-8"))
 
-print("Creating Cluster Now")
+banner("Creating Cluster...")
+os.system("sleep 2")
 
 os.system("kops create cluster -f cluster-config.yaml")
+print("{} has been Registered".format(clusterName))
+os.system("sleep 2")
+
+os.system("kops export kubeconfig --admin=18000h --name {}".format(clusterName))
+
+print("Applying the cluster changes")
+os.system("sleep 2")
 os.system("kops update cluster {} --yes".format(clusterName))
 
-# sp.getoutput()
-# os.system("echo {} is my cluster name".format(clusterName))
